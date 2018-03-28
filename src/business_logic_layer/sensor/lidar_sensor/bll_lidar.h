a08ad79fbcc9d9b62181f85da30895dd26495da8
leon-luo <luozhijy@163.com>
2018-03-28 18:13:41 +0800

    添加雷达业务处理相关功能

--- src/business_logic_layer/sensor/lidar_sensor/bll_lidar.h
+++ src/business_logic_layer/sensor/lidar_sensor/bll_lidar.h
@@ -1,31 +1,44 @@
 /******************************************************************************
 
-  Copyright (C), 2017-2028, 
+  Copyright (C), 2017-2028, HUIZHOU BLUEWAY ELECTRONICS Co., Ltd.
 
 *****************************************************************************
-  File Name      : bll_laser_radar.cpp
+  File Name      : bll_lidar.h
   Version        : Initial Draft
   Author         : Leon
-  Created        : 2018/3/22
+  Created        : 2018/3/23
   Last Modified  :
-  Description    : 
+  Description    : bll_lidar.cpp header file
   Function List  :
   History        :
-  1.Date         : 2018/3/22
+  1.Date         : 2018/3/23
     Author       : Leon
     Modification : Created file
  ******************************************************************************/
+#ifndef __BLL_LIDAR_H__
+#define __BLL_LIDAR_H__
+
+/*******************************************************************************/
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+/*******************************************************************************/
 
 /******************************************************************************
  * include header files list
  ******************************************************************************/
+#include <pthread.h>
+#include "base_type.h"
+#include <sensor_msgs/LaserScan.h>
 
 /******************************************************************************
  * external variables
  ******************************************************************************/
 
 /******************************************************************************
- * external function  difinition
+ * external function  prototypes
  ******************************************************************************/
 
 /******************************************************************************
@@ -49,92 +62,46 @@
  ******************************************************************************/
 
 /******************************************************************************
- * class difinition
+ * class prototypes
  ******************************************************************************/
+class bll_lidar
+{
+public:
+	bll_lidar();
+	~bll_lidar();
+private:
+};
+
+class bll_lidar
+{
+protected:
+	bll_lidar();
+	~bll_lidar();
+	
+public:
+	static bll_lidar* get_instance(void);
+	static void release_instance(void);
+
+	double get_directional_distance(const sensor_msgs::LaserScan &scan, double direction);
+
+private:
+	bll_lidar(const bll_lidar&){};
+	bll_lidar& operator=(const bll_lidar&){};
+	
+	static bll_lidar* p_instance_;
+	static pthread_mutex_t mutex_;
+};
 
 /******************************************************************************
- * internal function difinition
+ * internal function prototypes
  ******************************************************************************/
-pthread_mutex_t bll_lidar::mutex_;
-bll_lidar* bll_lidar::p_instance_ = nullptr;
 
-bll_lidar::bll_lidar()
-{
-
+/*******************************************************************************/
+#ifdef __cplusplus
+#if __cplusplus
 }
+#endif
+#endif /* __cplusplus */
+/*******************************************************************************/
 
-bll_lidar::~bll_lidar()
-{
-
-}
-
-bll_lidar* bll_lidar::get_instance(void)
-{
-	if (nullptr == p_instance_)
-	{
-		pthread_mutex_lock(&mutex_);
-		if (nullptr == p_instance_)
-		{
-			p_instance_ = new bll_lidar();
-		}
-		pthread_mutex_unlock(&mutex_);
-	}
-	return p_instance_;
-}
-
-void bll_lidar::release_instance(void)
-{
-	if (nullptr != p_instance_)
-	{
-		pthread_mutex_lock(&mutex_);
-		if (nullptr != p_instance_)
-		{
-			delete p_instance_;
-			p_instance_ = nullptr;
-		}
-		pthread_mutex_unlock(&mutex_);
-	}
-}
-
-double bll_lidar::get_directional_distance(const sensor_msgs::LaserScan & scan, double direction)
-{
-	double angle = direction;
-	double retDis = 0.0;
-	double dis = 0.0;
-	double minDis = 0.0;
-	int count = 0;
-	double angleScope = 0.0;
-	float32
-
-	//compute the valid value between angle-1.5 ~ angle+1.5 
-	double begin = D2R((angle - 1.5));
-	double end = D2R((angle + 1.5));
-
-	angleScope = scan.angle_max - scan.angle_min;
-	size_t ibegin = (size_t) ((begin - scan.angle_min) / angleScope * scan.ranges.size());
-	size_t iend = (size_t) ((end - scan.angle_min) / angleScope * scan.ranges.size());
-
-	minDis = scan.range_max;
-
-	for (size_t i = ibegin; i < iend; i++)
-	{
-		//filter the invalid value
-		if (scan.ranges[i] < scan.range_min || scan.ranges[i] > scan.range_max)
-		{
-			continue;
-		}
-
-		//get the min value
-		if (minDis > scan.ranges[i])
-		{
-			minDis = scan.ranges[i];
-		}
-	}
-
-	//the mini value as ranging value and returned
-	retDis = minDis;
-
-	return minDis;
-}
-
-
+#endif /* __BLL_LIDAR_H__ */
